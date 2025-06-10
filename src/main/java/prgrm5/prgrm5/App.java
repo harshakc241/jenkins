5af26ee3a0bb409c6602c175cf4aa3064616fc2e
Harsha-KC <harshakc080@gmail.com>
2025-06-10 07:25:29 +0530

    initial commit

---
+++ src/main/java/prgrm5/prgrm5/App.java
@@ -0,0 +1,13 @@
+package prgrm5.prgrm5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
